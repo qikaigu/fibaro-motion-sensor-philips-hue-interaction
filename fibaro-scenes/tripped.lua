857f85b46292a1aafac0492c922dba31a0fd059c
qikaigu <qikai.gu88@gmail.com>
2018-02-01 15:11:06 +0100

    added fibaro scenes

---
+++ fibaro-scenes/tripped.lua
@@ -0,0 +1,33 @@
+--[[
+%% properties
+5 value
+%% events
+%% globals
+--]]
+
+local startSource = fibaro:getSourceTrigger();
+local http = net.HTTPClient();
+local url = 'http://192.168.0.14:5000/my_home/entrance_lights/1';  -- server entry point
+
+if (
+ ( tonumber(fibaro:getValue(5, "value")) > 0 )  -- get the motion sensor signal
+or
+startSource["type"] == "other"
+)
+then
+  http:request(
+    url,
+    {
+      options={
+        method='GET'
+      },
+      success = function(resp)
+        if resp.status == 200 then
+          fibaro:debug(resp.status)
+        else
+          fibaro:debug(resp.status)
+        end
+      end
+    }
+  );
+end
